0890e97e5e9a9b3b381c0c4df37e705665abef5a
jahyeang <qkrwpgud1121@gmail.com>
2024-10-16 17:50:17 +0900

    .

---
+++ LoginTamplate/View/LoginMainView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginMainView.swift
+//  LoginTamplate
+//
+//  Created by 박제형 on 10/16/24.
+//
+
+import Foundation
+import UIKit
+import FlexLayout
+import PinLayout
+
+class LoginMainView: UIViewController {
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        
+        
+    }
+}
---
+++ LoginTamplate/ViewModel/LoginTemplateVM.swift
@@ -0,0 +1,13 @@
+//
+//  LoginTemplateVM.swift
+//  LoginTamplate
+//
+//  Created by 박제형 on 10/16/24.
+//
+
+import Foundation
+import UIKit
+
+class LoginTemplateVM: UIView {
+    
+}
