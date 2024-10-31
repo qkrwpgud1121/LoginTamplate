02b0bf3dd7b9b7cf0ac3d9870e5b9dd5ef69aeab
jahyeang <qkrwpgud1121@gmail.com>
2024-10-31 17:21:30 +0900

    login view

--- LoginTamplateSwiftUI/ContentView.swift
+++ LoginTamplateSwiftUI/ContentView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var id: String = ""
+    @State var pwd: String = ""
+    
     var body: some View {
         VStack {
-            TextField(<#LocalizedStringKey#>, text: <#Binding<String>#>)
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
+            
+            TextField("Enter your ID", text: $id)
+                .padding()
+                .background(Color(uiColor: .secondarySystemBackground))
+            TextField("Enter your Password", text: $pwd)
+                .padding()
+                .background(Color(uiColor: .secondarySystemBackground))
+            
+            Button(action: {
+                
+                guard !id.isEmpty else { print("ID is empty"); return }
+                guard !pwd.isEmpty else { print("Password is empty"); return }
+                
+                print("ID: \(id), PWD: \(pwd)")
+            }) {
+                Text("Login")
+            }.padding()
+            
+            HStack {
+                
+                Label("", systemImage: "")
+                    .background(Color(uiColor: .secondarySystemBackground))
+                Label("or", systemImage: "")
+                    .foregroundStyle(.secondary)
+                Label("", systemImage: "")
+                    .background(Color(uiColor: .secondarySystemBackground))
+                    
+            }
+            
         }
         .padding()
     }
